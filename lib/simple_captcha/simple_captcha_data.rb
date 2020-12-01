a73e01aa38f078f11134fd433bee88b8708f3b6a
孙傲 <2534658059@qq.com>
2020-12-01 11:04:18 +0800

    fix bug：delete_all在rails5没有参数

--- lib/simple_captcha/simple_captcha_data.rb
+++ lib/simple_captcha/simple_captcha_data.rb
@@ -9,13 +9,19 @@ module SimpleCaptcha
       end
       
       def remove_data(key)
-        delete_all(["#{connection.quote_column_name(:key)} = ?", key])
+        # delete_all(["#{connection.quote_column_name(:key)} = ?", key])
+        # => wrong (wrong number of arguments (given 1, expected 0))
+
+        where(["#{connection.quote_column_name(:key)} = ?", key]).delete_all
+        #=> right
+
         clear_old_data(1.hour.ago)
       end
       
       def clear_old_data(time = 1.hour.ago)
         return unless Time === time
-        delete_all(["#{connection.quote_column_name(:updated_at)} < ?", time])
+       # delete_all(["#{connection.quote_column_name(:updated_at)} < ?", time])
+       where(["#{connection.quote_column_name(:updated_at)} < ?", time]).delete_all
       end
     end
   end
